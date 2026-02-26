c59ebb63943aa6d9ed6139f7cf7e599d63296bff
Siva290395 <sivaphudur@gmail.com>
2026-02-26 18:14:33 +0530

    version 2

--- src/main/java/com/devops/demo/HelloController.java
+++ src/main/java/com/devops/demo/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Deployment Success ✅";
+        return "New Version Deployed 🔥";";
     }
 }
